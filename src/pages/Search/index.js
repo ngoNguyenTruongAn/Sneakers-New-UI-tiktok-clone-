7977cd0e2ed7fc71e164ad1ff638fdc03265fa5d
ngoNguyenTruongAn <ngontruongan@dtu.edu.vn>
2025-08-28 17:48:28 +0700

    Done-Xay Dung Base

---
+++ src/pages/Search/index.js
@@ -0,0 +1,9 @@
+function Search() {
+    return (
+        <div>
+            <h1>Welcome to the Search Page</h1>
+        </div>
+    );
+}
+
+export default Search;
---
+++ src/pages/Upload/index.js
@@ -0,0 +1,9 @@
+function Upload() {
+    return (
+        <div>
+            <h1>Welcome to the Upload Page</h1>
+        </div>
+    );
+}
+
+export default Upload;
