8b257827c7f0633162eb3f7f35dfa4cb10f7f33d
ngoNguyenTruongAn <ngontruongan@dtu.edu.vn>
2025-08-31 14:01:24 +0700

    Done Popper/Wrapper UI

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,13 +1,29 @@
-
 import styles from './Header.module.scss';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleXmark, faSpinner, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 
 function Header() {
-    return <header className={styles.wrapper}>
-     <div className={styles.inner}>
-        
-     </div>
-    </header>;
+    return (
+        <header className={styles.wrapper}>
+            <div className={styles.inner}>
+                <div className={styles.logo}>
+                    <img className={styles.logo} src="/img/logo.png" alt="" />
+                </div>
+                <div className={styles.search}>
+                    <input placeholder="Search account and video" spellCheck={false} />
+                    <button className={styles['clear']}>
+                        <FontAwesomeIcon icon={faCircleXmark} />
+                    </button>
+                    <FontAwesomeIcon className={styles['loading']} icon={faSpinner} />
+                    <button className={styles['search-btn']}>
+                        <FontAwesomeIcon icon={faMagnifyingGlass} />
+                    </button>
+                </div>
+                <div className={styles.actions}>
+                </div>
+            </div>
+        </header>
+    );
 }
 
 export default Header;
